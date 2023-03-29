76570b84d999167ba2b65e6df13f5a0a6c8b068a
tihmihvik <tihmihvik@gmail.com>
2023-03-30 00:23:53 +0700

    Прицепили tablayout

--- app/src/main/java/ru/tihonov/MainActivity.kt
+++ app/src/main/java/ru/tihonov/MainActivity.kt
@@ -1,13 +1,27 @@
 package ru.tihonov
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
     private lateinit var adapter: NumberAdapter
     private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private val tabNames: Array<String> = arrayOf(
+        "№10",
+        "№12",
+        "№3",
+        "№5",
+        "№15",
+        "№7",
+        "№2",
+        "№9",
+        "№14",
+        "№1"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +29,11 @@ class MainActivity : FragmentActivity() {
         adapter = NumberAdapter(this)
         viewPager = findViewById(R.id.pager)
         viewPager.adapter = adapter
+
+        tabLayout = findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabNames[position]
+        }.attach()
+
     }
 }
